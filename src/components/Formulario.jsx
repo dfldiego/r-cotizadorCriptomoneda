782757b66d33d0e7206e54f5fa3dcf8440863f77
dfldiego <dfl.diego92@gmail.com>
2020-08-27 17:26:55 -0300

    estilos al error. styled component al error

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
+import Error from './Error'
 import useMoneda from '../hooks/useMoneda';
 import useCriptomoneda from '../hooks/useCriptomoneda';
 import axios from 'axios';
@@ -69,7 +70,7 @@ const Formulario = () => {
         <form
             onSubmit={handleSubmit}
         >
-            {error ? 'Hay un error' : null}
+            {error ? <Error mensaje="Todos los campos son obligatorios" /> : null}
             <SelectMonedas />
             <SelectCripto />
             <Boton
