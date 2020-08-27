5068589cd99d3da3ee4e8969693940b723297a83
dfldiego <dfl.diego92@gmail.com>
2020-08-27 18:09:19 -0300

    agregando estilos al resultado. agregando styled component al resultado.

--- src/components/Cotizacion.jsx
+++ src/components/Cotizacion.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const ResultadoDiv = styled.div`
+    color: #fff;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Info = styled.p`
+    font-size: 18px;
+
+    span {
+        font-weight: bold;
+    }
+`;
+
+const Precio = styled.p`
+    font-size: 30px;
+    span {
+        font-weight: bold;
+    }
+`;
 
 const Cotizacion = ({ resultado }) => {
     console.log(resultado);
     if (Object.keys(resultado).length === 0) return null;
     return (
-        <div>
-            <p>El precio es: <span>{resultado.PRICE}</span> </p>
-            <p>El precio más alto del dia: <span>{resultado.HIGHDAY}</span> </p>
-            <p>El precio mas bajo del dia: <span>{resultado.LOWDAY}</span> </p>
-            <p>Variación últimas 24 horas: <span>{resultado.CHANGEPCT24HOUR}</span> </p>
-            <p>Última Actualización: <span>{resultado.LASTUPDATE}</span> </p>
-        </div>
+        <ResultadoDiv>
+            <Precio>El precio es: <span>{resultado.PRICE}</span> </Precio>
+            <Info>El precio más alto del dia: <span>{resultado.HIGHDAY}</span> </Info>
+            <Info>El precio mas bajo del dia: <span>{resultado.LOWDAY}</span> </Info>
+            <Info>Variación últimas 24 horas: <span>{resultado.CHANGEPCT24HOUR}</span> </Info>
+            <Info>Última Actualización: <span>{resultado.LASTUPDATE}</span> </Info>
+        </ResultadoDiv>
     );
 }
 
